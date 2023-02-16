fda38b9b4bf3627efe50edb029a1b38af81dc106
Lincoln Going <lincoln.going@my.wheaton.edu>
2023-02-16 15:14:56 -0600

    All commands in parser, implemention of individual classes needs to be written

---
+++ game/TakeC.java
@@ -0,0 +1,15 @@
+package game;
+
+public class TakeC implements Command{
+
+	public TakeC () {
+		
+	}
+
+
+	public void call(String first, String second, int words, Game game) {
+		//add code here
+		
+	}
+	
+}
---
+++ game/UseC.java
@@ -0,0 +1,15 @@
+package game;
+
+public class UseC implements Command{
+
+	public UseC () {
+		
+	}
+
+
+	public void call(String first, String second, int words, Game game) {
+		//add code here
+		
+	}
+	
+}
