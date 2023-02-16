8d203dbca3e6ee3bedf2522dc7e7e53394eed6a0
Camden <camden.flannagan@my.wheaton.com>
2023-02-16 13:15:09 -0600

    Created a game progression class to keep track of game progression

---
+++ game/GameProgression.java
@@ -0,0 +1,23 @@
+/**
+ * GameProgression
+ * 
+ * Used to keep track of how far the player is into the game
+ */
+
+package game;
+
+import java.util.HashMap;
+
+public class GameProgression {
+
+	/**
+	 * The player's backpack, so that the environment knows what
+	 * the player has
+	 */
+	HashMap<String, Item> Backpack;
+	
+	public GameProgression() {
+		
+	}
+	
+}
