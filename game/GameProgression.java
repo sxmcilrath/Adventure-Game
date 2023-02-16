319baf57f393e39c6a34898ea5ca71ec2c79a894
Camden <camden.flannagan@my.wheaton.com>
2023-02-16 14:37:42 -0600

    Made a class to keep track of game progression and made four Signfolk npc's

--- game/GameProgression.java
+++ game/GameProgression.java
@@ -1,12 +1,15 @@
 /**
  * GameProgression
  * 
- * Used to keep track of how far the player is into the game
+ * Used to keep track of how far the player is into the game,
+ * using metrics such as backpack contents...
  */
 
 package game;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GameProgression {
 
@@ -14,10 +17,43 @@ public class GameProgression {
 	 * The player's backpack, so that the environment knows what
 	 * the player has
 	 */
-	HashMap<String, Item> Backpack;
+	private HashMap<String, Item> backpack;
 	
-	public GameProgression() {
-		
+	/**
+	 * A hashset to record which game checkpoints have been crossed
+	 */
+	private HashSet<String> checkpoints;
+	
+	/**
+	 * constructor for game progression
+	 * @param backpack
+	 */
+	public GameProgression(HashMap<String, Item> backpack) {
+		this.backpack = backpack;
+		checkpoints = new HashSet<String>();
 	}
 	
+	/**
+	 * returns the set of checkpoints crossed
+	 * @return checkpoints set
+	 */
+	public Set<String> getCheckpoints() {
+		return checkpoints;
+	}
+	
+	/**
+	 * stores which checkpoints have been crossed by the player
+	 * @param checkpoint The ckeckpoint that has been crossed
+	 */
+	public void checkpointCrossed(String checkpoint) {
+		checkpoints.add(checkpoint);
+	}
+
+	/**
+	 * getter for the contents of the backpack
+	 * @return the keyset of the backback
+	 */
+	public Set<String> getBackpack() {
+		return backpack.keySet();
+	}
 }
---
+++ game/SimeusSignon.java
@@ -0,0 +1,53 @@
+/**
+ * SimeusSignon
+ * 
+ * Simeus introduces combat to the game. The more excitable brother of Sineus
+ */
+
+package game;
+
+public class SimeusSignon implements SignFolk {
+
+	private String sword;
+	private String noSword;
+	private String swordAttacked;
+	private String noSwordAttacked;
+	
+	public SimeusSignon() {
+		sword = "Ah, yes! You have a sword! Why don't you give it go?";
+		noSword = "You really think you can take me down without a sword? I mean, you don't "
+				+ "have to fight me to move on, but it seems unwise to go forward if you aren't even "
+				+ "able to stand up to me.";
+		swordAttacked = "Alright, alright, cut it out! You've had enough tutorial with that death "
+				+ "stick. Get a move on.";
+		noSwordAttacked = "Ha. You fool. Have it you way, face my power";
+	}
+	
+	@Override
+	/**
+	 * What does Simeus say when talked to?
+	 * @param progression Do you or do you not have a sword
+	 * @return what Simeus says
+	 */
+	public String talk(GameProgression progression) {
+		if (progression.getBackpack().contains("sword")) {
+			return sword;
+		} else {
+			return noSword;
+		}
+	}
+	
+	/**
+	 * What should Simeus say when attacked?
+	 * @param progression Do you or do you not have a sword
+	 * @return his response
+	 */
+	public String attacked(GameProgression progression) {
+		if (progression.getBackpack().contains("sword")) {
+			return swordAttacked;
+		} else {
+			return noSwordAttacked;
+		}
+	}
+
+}
