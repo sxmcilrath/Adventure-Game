1ed51b087759d735c015bd5b2410f02cb05501c5
Camden <camden.flannagan@my.wheaton.com>
2023-03-17 21:06:20 -0500

    I think this time I'm really finished with Silvester's dialogue

--- game/SirainSignon.java
+++ game/SirainSignon.java
@@ -11,14 +11,14 @@ package game;
 public class SirainSignon extends NPC {
 
 	/**
-	 * the number of times Sirain has been talked to
+	 * the number of times Sirain has been talked to before talking to Simon
 	 */
-	private int numTalked = 0;
+	private int preSimonCounter = 0;
 	
 	/**
-	 * all the things that Sirain can say
+	 * all the things that Sirain can say before you talk to Simon
 	 */
-	private String[] says = {"What are you lookin' at me for? Pick a direction and go! How am I supposed to know "
+	private String[] preSimon = {"What are you lookin' at me for? Pick a direction and go! How am I supposed to know "
 			+ "where you should go? \nI've been in this spot my whole life! What, just 'cause you have "
 			+ "three options for which way to go, you're freakin' out? \nListen, if you're really "
 			+ "so bothered by not knowin' where to go, just head north and talk to Simon.", 
@@ -29,11 +29,23 @@ public class SirainSignon extends NPC {
 					+ "from now on."};
 	
 	/**
+	 * the number of times Sirain has been talked to after talking with Simon and before leaving the forest
+	 */
+	private int postSimonCounter;
+	
+	/**
+	 * All the things Sirain can say after talking to Simon
+	 */
+	private String[] postSimon = {
+		
+	};
+	
+	/**
 	 * Method for Sirain to talk to the player
 	 * @return Sirain's words of wisdom
 	 */
 	public String talk() {
-		return cycleTalk(numTalked++, says);
+		return cycleTalk(preSimonCounter++, preSimon);
 	}
 
 }
